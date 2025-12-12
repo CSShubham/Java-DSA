1d2d508917e46cb767d8531b078761b6828a66ac
CSShubham <raj079097@gmail.com>
2025-12-12 21:31:03 +0530

    bitmasking operation

---
+++ Basics/Bit_Manipulation/UpdateBitOp.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class UpdateBitOp {
+    public static void main(String[] args) {
+        int n =5;//0101
+        String str=Integer.toBinaryString(n);
+        System.out.println("initial number is: "+str);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter operation to perform: \n 1.set \n2.clear \n(enter : 1/2) :: )");
+        int oper = sc.nextInt();
+        System.out.println("enter position you want to operate :");
+        int pos=sc.nextInt();
+        if (pos>str.length()-1){
+            System.out.println("invalid position");
+        }
+        else{
+            int bitMask=1<<pos;
+            if(oper==1){
+                //set
+                int newNumber= bitMask | n ;
+                System.out.println("new Number is : "+ Integer.toBinaryString(newNumber) );
+            }
+            else if(oper==2){
+                int newNumber=(~(bitMask)&n);
+                System.out.println("new Number is : "+ Integer.toBinaryString(newNumber) );
+            }
+        
+        }
+    }
+}
