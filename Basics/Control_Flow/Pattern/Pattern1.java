25a03c8214dac5fa6c8be55f8e247186b32ab362
CSShubham <raj079097@gmail.com>
2025-12-13 21:34:33 +0530

    PyramidPattern

---
+++ Basics/Control_Flow/Pattern/Pattern1.java
@@ -0,0 +1,15 @@
+package Basics.Control_Flow.Pattern;
+
+public class Pattern1 {
+    // basic pyramid
+    public static void main(String[] args) {
+        System.out.println("");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
